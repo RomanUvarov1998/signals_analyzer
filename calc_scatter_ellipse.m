b16deb5cc322be64e3985d129b39bc9b04c205a5
Roman Uvarov <uvarovrs40@gmail.com>
2022-04-10 07:12:22 +0300

    proper ellipse

--- calc_scatter_ellipse.m
+++ calc_scatter_ellipse.m
@@ -1,68 +1,51 @@
-function [sc_x, sc_y, el_x, el_y] = calc_scatter_ellipse(intervals)
+function [sc_x, sc_y, el_x, el_y, el_params] = calc_scatter_ellipse(intervals)
     sc_x = intervals(1 : end - 1);
     sc_y = intervals(2 : end);
     
-    % Аппроксимируем линейной регрессией
-    x = [sc_x'; ones(1, length(sc_y))];
-    k = sc_y' / x;
+    % Магическим образом находим матрицу эллипса и его центр
+    % Код украден с:
+    % https://viewer.mathworks.com/?viewer=plain_code&url=https%3A%2F%2Fwww.mathworks.com%2Fmatlabcentral%2Fmlc-downloads%2Fdownloads%2Fsubmissions%2F9542%2Fversions%2F3%2Fcontents%2FMinVolEllipse.m&embed=web
+    [A, c] = MinVolEllipse([sc_x, sc_y]', 0.01);
     
-%     figure(4), cla, hold on, grid on;
-%     xlim([0, max(sc_x) + 1]);
-%     ylim([0, max(sc_y) + 1]);
+    % Собственные числа и вектора матрица А
+    [V, D] = eig(A);
     
-    % Найдем угол наклона прямой
-    theta = atand(k(1));
+    % Полуоси эллипса
+    a = 1 / sqrt(D(1, 1));
+    b = 1 / sqrt(D(2, 2));
     
-    % Повернем точки скаттерограммы вокруг (0; 0) по часовой стрелке,
-    % чтобы полуоси эллипса были параллельны осям координат
-    R = [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
-    pts = [sc_x, sc_y] * R;
-    
-%     scatter(pts(:, 1), pts(:, 2), 'r');
-    
-    % Найдем описывающий прямоуольник
-    max_pt = max(pts);
-    min_pt = min(pts);
-    
-    % Найдем центр описывающего прямоуольника
-    center = (max_pt + min_pt) ./ 2;
-%     plot(center(1), center(2), '*g');
-    ab = max(pts - center);
-    
-    % Найдем полуоси как половины сторон описывающего прямоуольника
-    a = ab(1);
-    b = ab(2);
-    
-    % Создалим точки эллипса по его уравнению
+    % Множество точек Х у неповернутого эллипса с центом (0; 0)
     el_x = linspace(-a, a, 1000);
+    % Множество точек Y у неповернутого эллипса с центом (0; 0)
     el_y = b .* (1 - (el_x ./ a) .^ 2) .^ 0.5;
+    % Вторая половина эллипса
     el_x = [el_x, flip(el_x)];
     el_y = [el_y, -flip(el_y)];
     
-    % Сместим его к центру повернутой скаттерограммы
-    el_x = el_x + center(1);
-    el_y = el_y + center(2);
-    
-    el_pts = [el_x; el_y];
-    
-%     plot(el_x, el_y, 'b');
-    
-    % Повернем эллипс обратно вокруг точки (0; 0) против часовой стрелки
-    theta = -atand(k(1));
+    % Угол поворота эллипса
+    theta = -atand(V(1, 2) / V(1, 1));
+    % Матрица поворота эллипса
     R = [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
     
-    el_pts = el_pts' * R;
+    % Поворачиваем эллипс
+    el_pts = [el_x; el_y]';
+    el_pts = el_pts * R;
     
     el_x = el_pts(:, 1);
     el_y = el_pts(:, 2);
-
-%     scatter(sc_x, sc_y, 'b');
-%     plot(el_x, el_y, 'r');
-
-%     center = center * R;
-%     plot(center(1), center(2), '*y');
-% 
-%     plot(max_pt(1), max_pt(2), '*g');
-%     plot(min_pt(1), min_pt(2), '*k');
+    
+    % Устанавливаем центр эллипса
+    el_x = el_x + c(1);
+    el_y = el_y + c(2);
+    
+    % Параметры эллипса
+    el_params.a = a; 
+    el_params.b = b;
+    el_params.square = pi * a * b; 
+    el_params.m_sr = mean(intervals); 
+    el_params.interv_min = min(intervals); 
+    el_params.interv_max = max(intervals); 
+    el_params.interv_range = el_params.interv_max - el_params.interv_min;
+    el_params.mo = mode(intervals);
 end
 
